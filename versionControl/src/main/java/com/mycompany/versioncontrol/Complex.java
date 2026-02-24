41867c50ed32db8815ae4dc95ff8cb8b05b46c9c
nd24aar <nd24aar@herts.ac.uk>
2026-02-24 14:02:27 +0000

    added the constructor to the complex class

--- versionControl/src/main/java/com/mycompany/versioncontrol/Complex.java
+++ versionControl/src/main/java/com/mycompany/versioncontrol/Complex.java
@@ -1,5 +1,11 @@
 package com.mycompany.versioncontrol;
 
 public class Complex {
+    private double real;
+    private double imaginary;
     
+    public Complex(double real, double imaginary){
+        this.real = real;
+        this.imaginary = imaginary;
+    }
 }
