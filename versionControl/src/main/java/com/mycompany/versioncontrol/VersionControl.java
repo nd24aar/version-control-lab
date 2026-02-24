780f6bfdb72af8b13593768c456fe9c74385e45c
nd24aar <nd24aar@herts.ac.uk>
2026-02-24 14:14:21 +0000

    test2

--- versionControl/src/main/java/com/mycompany/versioncontrol/VersionControl.java
+++ versionControl/src/main/java/com/mycompany/versioncontrol/VersionControl.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.versioncontrol;
 
-/**
- *
- * @author ickleberry
- */
 public class VersionControl {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Ellie!");
     }
 }
